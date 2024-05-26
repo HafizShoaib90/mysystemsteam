35faa45c77c78e1f81abde38d22149ec9fd13c0b
IchbinShoaib <85681857+HafizShoaib90@users.noreply.github.com>
2024-05-27 02:38:43 +0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,45 @@
+
+provider "aws" {
+  region = "us-east-1"  
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias= "ohio"  
+}
+
+resource "aws_vpc" "mainvpc" {
+
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "prod" {
+    vpc_id = aws_vpc.mainvpc.id
+    cidr_block= "10.0.1.0/24" 
+}
+
+resource "aws_subnet" "prod" {
+    vpc_id = aws_vpc.mainvpc.id
+    cidr_block= "10.0.2.0/24" 
+}
+
+
+resource "aws_vpc" "mainvpc" {
+
+    cidr_block = "10.0.0.0/16"
+    provider = "aws.ohio"
+}
+
+resource "aws_subnet" "prod" {
+    vpc_id = aws_vpc.mainvpc.id
+    cidr_block= "10.0.1.0/24"
+    provider = "aws.ohio" 
+}
+
+resource "aws_subnet" "prod" {
+    vpc_id = aws_vpc.mainvpc.id
+    cidr_block= "10.0.2.0/24"
+    provider = "aws.ohio" 
+}
+
+
